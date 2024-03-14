dbf5bebb82c67f6e601a89c86973cfeeb2a08a4a
rohit-workspace <rohit.chaudhari@v2solutions.com>
2024-03-14 15:35:15 +0530

    redux-toolkit e-cart mobile accessaries

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBBtn,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getCartTotal } from "../features/cartSlice";
+
+export default function Navbar() {
+    const { cart, totalQuantity } = useSelector((state) => state.allCart);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCartTotal());
+    }, [cart]);
+
+    return (
+        <MDBNavbar light bgColor="light">
+            <MDBContainer fluid>
+                <MDBNavbarBrand color="red">E-Cart</MDBNavbarBrand>
+                <MDBBtn color="dark">
+                    <Link to="/">All Products</Link>
+                </MDBBtn>
+
+                <MDBBtn color="light">
+                    <Link to="/cart">Cart({totalQuantity})</Link>
+                </MDBBtn>
+            </MDBContainer>
+        </MDBNavbar>
+    );
+}
